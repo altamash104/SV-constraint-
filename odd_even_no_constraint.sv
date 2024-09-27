590178166c6f4586d9129daa9dc178de92f099d4
altamash104 <altamashshahid93@gmail.com>
2024-09-27 15:14:50 +0530

    Update odd_even_no_constraint.sv

--- odd_even_no_constraint.sv
+++ odd_even_no_constraint.sv
@@ -1,14 +1,39 @@
 
-//constraint for odd & even number generation
+//constraint for odd number
 
 class test;
   
   rand bit [4:0] a[];
   integer i;
   constraint x{a.size()==10;};
-  //constraint y{foreach (a[i]) for odd numbers
-    //a[i]%2==1;};
-  constraint z{foreach (a[i]) //even numbers
+  constraint y{foreach (a[i])
+    a[i]%2==1;};
+  
+  function void post_randomize();
+    $display("%0p",a);
+  endfunction
+  
+endclass
+  
+module my_class;
+  
+  test t;
+  
+  initial
+    begin
+      repeat(10)
+      t=new();
+      t.randomize();
+    end
+endmodule
+
+//constraint for even number
+class test;
+  
+  rand bit [4:0] a[];
+  integer i;
+  constraint x{a.size()==10;};
+  constraint z{foreach (a[i]) 
     a[i]%2==0;};
   
   function void post_randomize();
