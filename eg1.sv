3cbe3056e5a6f04cc8df29e483325af689aa5a0f
Altamash Shahid <altamashshahid93@gmail.com>
2024-10-02 00:24:07 +0530

    Create eg1.sv
    generate unique value without using rand or randc value

---
+++ eg1.sv
@@ -0,0 +1,39 @@
+class packet;
+
+	bit[3:0]a;
+	int q[$];
+	
+	function void post_randomize();
+		
+		q.push_back(a);
+		$display("value of a =%0d",a);
+	endfunction
+	
+	constraint c{foreach (q[i])
+		a!=q[i];
+		}
+		
+endclass
+
+module top;
+
+packet p;
+	initial
+		begin
+		p=new;
+		
+		for(int i=0;i<15;i++)
+			begin
+        if(i==0)
+			begin
+			    p.c.constraint_mode(0);
+			    p.randomize(a);
+			end
+			else
+			  begin
+			    p.c.constraint_mode(1);
+			    p.randomize(a);
+			  end
+		end
+	end
+endmodule
