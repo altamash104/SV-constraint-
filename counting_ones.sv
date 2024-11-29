8f9e3cbcd92d032c2408516783ec9136e140b071
Altamash Shahid <altamashshahid93@gmail.com>
2024-11-29 16:03:58 +0530

    Create counting_ones.sv
    counting ones without using $countones

---
+++ counting_ones.sv
@@ -0,0 +1,28 @@
+class test;
+  rand bit[7:0] a;
+  int count_ones;
+
+  function void post_randomize();
+    count_ones =0;
+    foreach(a[i])
+      if(a[i]==1)
+        count_ones++;
+  endfunction
+  
+
+endclass
+
+module top;
+  
+  test t;
+  initial 
+    begin
+      t=new();
+      repeat(5)
+        begin
+        t.randomize();
+        $display("a=%0b count_ones=%0d",t.a,t.count_ones);
+        end
+      
+     end
+endmodule
