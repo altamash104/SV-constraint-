8b46d9f8e149b4c005c931d65f4533f116ff8f1d
Altamash Shahid <altamashshahid93@gmail.com>
2025-04-27 14:34:01 +0530

    Create the series of 2 power without using the power and mul opertaor.sv

---
+++ the series of 2 power without using the power and mul opertaor.sv
@@ -0,0 +1,22 @@
+class test;
+
+  rand bit [7:0] a [8];
+  constraint c {
+    foreach (a[i]) 
+      a[i] == (1 << i);
+  }
+
+  function void post_randomize();
+    foreach (a[i])
+      $display("a[%0d] = %0d", i, a[i]);
+  endfunction
+endclass
+module top;
+      test t;
+      initial
+        begin
+        
+          t=new;
+          t.randomize();
+        end
+  endmodule
