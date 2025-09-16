c29877cc7e84c35b94a639eb36b6f6460fa57fb9
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-16 14:49:08 +0530

    Update fibonacci_series.sv

--- fibonacci_series.sv
+++ fibonacci_series.sv
@@ -30,3 +30,40 @@ module test;
         end
     end
 endmodule
+
+//Another method without randomization(more accurate)
+class fibonacci;
+  int a[];
+
+  function new(int n);
+    a = new[n];
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 2; i < n; i++)
+      a[i] = a[i-1] + a[i-2];
+  endfunction
+  // function void build(int n);
+  //   a = new[n];
+  //   a[0] = 0;
+  //   a[1] = 1;
+  //   for (int i = 2; i < n; i++)
+  //     a[i] = a[i-1] + a[i-2];
+  // endfunction
+  /*
+  if we do not want to use in built constructor then we have to use our own function(build).
+  note- when we want print a series then we have to use function who do not return single number .that is why we use void function.
+  */
+
+  function void display();
+    $display("%0p", a);
+  endfunction
+endclass
+
+module test;
+  fibonacci t;
+  initial begin
+    t = new(10);
+    // h.build(10);
+    t.display();
+  end
+endmodule 
