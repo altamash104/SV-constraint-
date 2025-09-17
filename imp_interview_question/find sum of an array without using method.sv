a94a05caf7684e31378849824843205e04c474bf
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-17 15:30:26 +0530

    Update find sum of an array without using method.sv

--- imp_interview_question/find sum of an array without using method.sv
+++ imp_interview_question/find sum of an array without using method.sv
@@ -14,4 +14,28 @@ module test;
       $display("sum of an array=%0d",sum);
     end
 endmodule
-
+///////////////////////////////
+//using function
+module test;
+  int da[];
+  int sum;
+  
+  function void sum_array(input int da[],output int sum);
+      begin
+        sum=0;
+      for(int i=0;i<da.size;i++)begin
+        sum=sum+da[i];
+      end
+    end
+  endfunction
+  
+  initial
+    begin
+      da=new[6];
+      da='{1,2,3,4,5,6};
+      
+      sum_array(da,sum);
+      $display("sum of an array=%0d",sum);
+    end
+	
+endmodule
