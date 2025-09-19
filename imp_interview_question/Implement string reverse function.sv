c2509dbe8f342abe2fc9ab80ab72c2c26c96259d
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-19 15:13:59 +0530

    Update Implement string reverse function.sv

--- imp_interview_question/Implement string reverse function.sv
+++ imp_interview_question/Implement string reverse function.sv
@@ -1,3 +1,22 @@
+module test;
+  string s1 = "altamash";
+  string s2 = "";
+
+  function string reverse(input string str);
+    string rev = "";
+    for (int i = str.len()-1; i >= 0; i--) begin
+      rev = {rev, str[i]}; 
+    end
+    return rev;
+  endfunction
+
+  initial begin
+    s2 = reverse(s1);
+    $display("Original string = %s", s1);
+    $display("Reversed string = %s", s2);
+  end
+endmodule
+//////////////////////////////////////////////////////
 module test; 
 	string s1 = "altamash"; 
 	string s2; 
