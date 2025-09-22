bd070348dde8a82c019921773f2887c03d57382f
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-22 11:33:52 +0530

    Update Check if a string is a palindrome.sv

--- imp_interview_question/Check if a string is a palindrome.sv
+++ imp_interview_question/Check if a string is a palindrome.sv
@@ -30,3 +30,18 @@ module test;
         $display("%s is not palindrome",s);
     end
 endmodule
+
+//shorcut method
+module test;
+  string s="MADAM";
+  string rev;
+  
+  initial
+    begin
+      rev={<<8{s}};//this streaming operator reverse the string
+      if(s==rev)
+        $display("String is palindrome=%s",s);
+      else
+        $display("String is palindrome=%s",s);
+    end
+endmodule
