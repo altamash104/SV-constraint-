4ddbf533fb00016f396daef71b1b2a68d2e8c236
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-23 12:27:39 +0530

    Create write constraint to find the sum of all element in a matrix.sv

---
+++ 2D-Constraint/write constraint to find the sum of all element in a matrix.sv
@@ -0,0 +1,32 @@
+class test;
+  rand int a[4][4];
+  int sum;
+  
+  constraint c1 {foreach(a[i,j]) a[i][j] inside {[1:10]};}
+  
+  function void post_randomize();
+    sum=0;
+    foreach(a[i])
+      begin
+        foreach(a[i,j])
+          begin
+            $write("%0d",a[i][j]);
+          end
+        $display("");
+      end
+     
+    foreach(a[i,j])
+      sum=sum+a[i][j];
+    $display("sum=%0d",sum);
+  endfunction
+endclass
+
+module top;
+  test t;
+  
+  initial
+    begin
+      t=new;
+      t.randomize();
+      end
+endmodule
