b0fb477a3830d99a3131c13916dad4dd330bafb2
Altamash Shahid <altamashshahid93@gmail.com>
2025-09-25 12:18:30 +0530

    Create right aligned pyramid.sv

---
+++ Pattern coding/right aligned pyramid.sv
@@ -0,0 +1,17 @@
+module test;
+  initial
+    begin
+      int n=4;
+      for(int i=1;i<=n;i++)begin
+        
+        for(int j=1;j<=n-i;j++)begin
+          $write(" ");
+        end
+        
+        for(int k=1;k<=i;k++)begin
+          $write("*");
+        end
+        $display("");
+      end
+    end
+endmodule
